0ef42251e352e753dab261db3de661f59609892c
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-03-27 20:54:22 +0200

    Created constructor

---
+++ script.js
@@ -0,0 +1,20 @@
+const bookLibrary = [];
+
+function Book(title, author, pages, read) {
+    if(!new.target) {
+        throw Error('You must use the "new" operator to call the constructor')
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    };
+
+function addBookToLibrary() {
+    let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
+    bookLibrary.push(theHobbit)
+}
+// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+// console.log(theHobbit.info());
+// //"The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+addBookToLibrary();
