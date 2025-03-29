2ba4fb03e7fc569f73d18f1f0fcd4e7b0e4b49c1
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-03-29 18:41:57 +0200

    Testing different solutions to change object name

--- script.js
+++ script.js
@@ -10,11 +10,13 @@ function Book(title, author, pages, read) {
     this.read = read;
     };
 
+
+
 function addBookToLibrary(title, author, pages, read) {
-    let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
-    bookLibrary.push(theHobbit)
+    const theBook = new Book(title, author, pages, read);
+    bookLibrary.push(theBook);
+    
 }
-// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
-// console.log(theHobbit.info());
+
 // //"The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-addBookToLibrary();
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
