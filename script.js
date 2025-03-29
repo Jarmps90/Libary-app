c191ee71fadc4cdfaac5225f4f63b62ad6bbb893
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-03-29 21:13:09 +0200

    Finded way how to add id to the book objects

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ function Book(title, author, pages, read) {
     if(!new.target) {
         throw Error('You must use the "new" operator to call the constructor')
     }
+    
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     };
 
-
-
 function addBookToLibrary(title, author, pages, read) {
-    const theBook = new Book(title, author, pages, read);
-    bookLibrary.push(theBook);
+    const newBook = new Book(title, author, pages, read);
+    bookLibrary.push(newBook);
+    bookLibrary.forEach((item) => {item.id = self.crypto.randomUUID()});
+};
+
     
-}
+
 
 // //"The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+
+console.log(bookLibrary);
