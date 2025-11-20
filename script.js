51e6f5a6cc33d6695e11c4fc3c4f9eb2920f1c4e
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-11-20 19:57:46 +0200

    Statring refactor js code. Added some pseudo code

--- script.js
+++ script.js
@@ -17,6 +17,17 @@ function Book(title, author, pages, read) {
     this.read = read;
 };
 
+// class Book {
+ 
+//   constructor(title, author, pages, read) {
+//    
+//    this.title = title;
+//    this.author = author;
+//    this.pages = pages;
+//    this.read = read;
+// }
+// };
+
 function displayBook(book) {
   container.innerHTML = '';
   
